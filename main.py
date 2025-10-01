8f3ca422aaebcc8c183945367eb0e1762f6f80f6
Stefano Silva <stefano.silva1994@gmail.com>
2025-10-01 16:32:40 +0100

    Updated docker file and main.py to test selenium-chrome-integration

--- main.py
+++ main.py
@@ -2,10 +2,13 @@
 import boto3
 from scrapers.scraper_constants import ScraperConstants as sc
 from utils.s3_utils import rename_file_in_s3, is_running_in_aws
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 if __name__ == '__main__':
     print('Hello World')
+    # Script to run selenium with chromedriver - to test running it in a docker image
 
     # Get env variable:
     env = is_running_in_aws()
@@ -19,11 +22,28 @@ if __name__ == '__main__':
     # Initialize S3 client
     s3 = session.client('s3')
 
-    response =  s3.list_objects(
-        Bucket=sc.S3_BUCKET_NAME,
-    )
-    content = response["Contents"]
 
-    rename_file_in_s3(s3, sc.S3_BUCKET_NAME, "raw/fbref_data/2022-08-05_2023-05-28.csv", "raw/fbref_data/2022-2023.csv")
-
+    url = "https://fbref.com/en/comps/9/2024-2025/schedule/2024-2025-Premier-League-Scores-and-Fixtures"
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # keep it headless if you want
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("start-maximized")
+    chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                                "AppleWebKit/537.36 (KHTML, like Gecko) "
+                                "Chrome/116.0.5845.96 Safari/537.36")
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get(url)
+    try:
+        driver.get(url)
+        html = driver.page_source
+        print("✅ Page title is:", driver.title)
+        print("✅ Page loaded successfully")
+    except Exception as e:
+        print("❌ Error:", e)
+        print("Why, Why, Why")
+    finally:
+        driver.quit()
 
